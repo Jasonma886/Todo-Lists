5c85aad5370605ec0ede999d69845e348351822c
jason <jasonma886@gmail.com>
2018-08-01 11:00:20 +0700

    package setting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
+    <van-button>for test</van-button>
     <ul>
       <li>
         <a
@@ -95,10 +96,10 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
+<style lang="stylus" scoped>
+h1, h2
   font-weight: normal;
-}
+
 ul {
   list-style-type: none;
   padding: 0;
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/login'
+import Home from '@/components/index'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {unAuth: true}
+    },
+    {
+      path: '/hello',
+      name: 'hello',
       component: HelloWorld
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  console.log(to)
+  next()
+})
+
+export default router

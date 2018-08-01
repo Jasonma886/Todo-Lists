95e3949a6b841322573792fbdf3ca30bc39e8a5f
jason <jasonma886@gmail.com>
2018-08-01 17:36:30 +0700

    store managing

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vant from 'vant'
+import store from './store'
 import 'vant/lib/vant-css/index.css'
 
 Vue.config.productionTip = false
@@ -13,6 +14,7 @@ Vue.use(Vant)
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
